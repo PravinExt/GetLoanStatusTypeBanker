f441ec62bd39b270b72ac92ab21a2192597db6ea
PravinDalavi <pravin.dalavi@gmail.com>
2021-10-11 17:42:15 +0530

    First commit

---
+++ GetLoanStatusTypeBanker/LoanStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetLoanStatusTypeBanker
+{
+    public class LoanStatus
+    {
+        public string Status_ID { get; set; }
+        public int Status { get; set; }
+
+    }
+
+    public enum BankerLoanStaus : int
+    {
+        Submitted = 1,
+        InReview = 2,
+        Rejected = 3,
+        MoreInfoRequested = 4,
+        ApprovedForCredit = 5
+    }
+}
